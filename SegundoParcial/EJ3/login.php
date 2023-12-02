b22a094060dbc90123b3707b3ecd44fa23f997aa
VsEd <eddy.navia.405@gmail.com>
2023-12-01 23:41:53 -0400

    maquetacion final

--- SegundoParcial/EJ3/login.php
+++ SegundoParcial/EJ3/login.php
@@ -1,6 +1,30 @@
-<?php
-session_start();
-include("conexion.php");
-
-
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="javascript:autenticar()">
+        <div>
+            <label for="usuario">Usuario: </label>
+            <input type="text" name="usuario" id="usuario">
+        </div>
+        <div>
+            <label for="contrasena">Contraseña: </label>
+            <input type="password" name="contrasena" id="contrasena">
+        </div>
+        <div>
+            <img src="http://localhost/DesarrolloWebENC/SegundoParcial/EJ3/capcha.php" alt="captcha.jpg">
+        </div>
+        <div>
+            <label for="capcha">Ingrese el captcha:</label>
+            <input type="text" name="capcha" id="capcha">
+        </div>
+        <div>
+            <input type="submit" value="Ingresar">
+        </div>
+    </form>
+</body>
+</html>
